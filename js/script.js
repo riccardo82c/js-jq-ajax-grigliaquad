12b766e09dd941ad524c68285e8394dd9b23d8f6
Riccardo Colombo <rikyregna@gmail.com>
2020-08-27 21:26:46 +0200

    comment and createGrid function()

--- js/script.js
+++ js/script.js
@@ -2,44 +2,57 @@
 Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
 Il numero ottenuto appare al centro del quadrato. */
 
-
-
 $(function () {
 
+
+	// metodo on('click') mi permette di definire un evento ad un elemento, tale evento può essere associato anche ad un elemento che non è ancora stato creato, poichè on permette di RESTARE IN ATTESA della creazione della griglia.
 	$('.container').on('click', '.square', function () {
 
+		/* Setto $(this) in una variabile poichè se la dichiarassi dentro la chiamata AJAX andrebbe a prendere il valore dell'oggetto della chiamata*/
 		let quadrato = $(this);
 
-
+		// Se quadrato NON ha la classe clicked...
 		if (!quadrato.hasClass('clicked')) {
-
+			// Chiamata ad AJAX
 			$.ajax({
 				'url': "https://flynn.boolean.careers/exercises/api/random/int",
 				'method': "GET",
-				'success': function (data, text, jq) {
+				// In caso di success della chiamata chiamo la funzione che come parametri mi restituisce data (valore restituito dalla chiamata)
+				'success': function (data, stato, jq) {
+					console.log(stato);
+					console.log(jq.status);
 					retrieveNumber(data, quadrato);
 				},
-				'error': function () {
-					alert("E' avvenuto un errore. ");
+				// In caso di errore
+				'error': function (richiesta, stato, errori) {
+					console.log(richiesta.status);
+					console.log(stato);
+					console.log(errori);
+					alert("E' avvenuto un errore.");
 				}
 			});
 
+			// Se quadrato HA la classe clicked...
 		} else {
+			// richiamo la funzione fading per avviare l'animazione
 			fading(quadrato);
 			console.log('already clicked');
 		}
-
-
-
 	});
 
-	for (let i = 0; i < 36; i++) {
-		$('.container').append('<div class="square"></div>');
-	}
+	// Creo la griglia tramite .append(template string). Posso invocare la creazione della griglia senza problemi poichè uso on('click') partendo dall'elemento genitore (esistente) sull'elemento figlio (ancora da creare). Ascoltatore di eventi
 
-
+	createGrid();
 
 	/* funzioni */
+
+
+	function createGrid() {
+		for (let i = 0; i < 36; i++) {
+			$('.container').append('<div class="square"></div>');
+		}
+
+	}
 
 	function retrieveNumber(obj, element) {
 		element.addClass('clicked');
@@ -50,26 +63,10 @@ $(function () {
 
 
 	function fading(element) {
-		for (let i = 0; i < 6; i++) {
-			element.fadeOut(80);
-			element.fadeIn(80);
+		for (let i = 0; i < 2; i++) {
+			element.fadeOut(100);
+			element.fadeIn(100);
 		}
 	}
-	/* 
-		function animation(element) {
-
-			element.animate({
-				scale: 1.1,
-
-				opacity: 0.4
-
-			}, 200);
-
-			element.animate({
-				scale: 1,
-				opacity: 1
-
-			}, 200);
-		} */
 
 });
